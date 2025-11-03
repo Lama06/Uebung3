409acf0d5dc019b6b4b512bf55f44d2bde5a25f7
Andreas Prues <andreasprues36@gmail.com>
2025-11-03 13:13:38 +0100

    Aufgabe 3

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,13 +2,28 @@ package h3;
 
 public class H3_main {
     public static void main(String[] args) {
-        int max = 0;
-        int fix = 0;
-        int wartend = 0;
+        int max = 5;
+        int fix = 2;
+        int wartend = 2;
         boolean istVoll = false;
 
-        if (fix < max) {
+        int frei = max - fix;
+        if (wartend < frei) {
             fix += wartend;
+            wartend = 0;
+            istVoll = false;
+        } else if (wartend == frei) {
+            fix = max;
+            wartend = 0;
+            istVoll = true;
+        } else { // wartend > frei
+            fix = max;
+            wartend -= frei;
+            istVoll = true;
         }
+
+        System.out.println(fix);
+        System.out.println(wartend);
+        System.out.println(istVoll);
     }
 }
